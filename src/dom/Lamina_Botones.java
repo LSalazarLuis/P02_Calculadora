33a9d5526bec114cd2f135de40a0ca67ada5ea7b
LSalazarL <luis.salazar.ing@gmail.com>
2023-07-31 14:09:04 +0200

    Se crean los botones de la calculadora

--- src/dom/Lamina_Botones.java
+++ src/dom/Lamina_Botones.java
@@ -1,5 +1,30 @@
 package dom;
+import javax.swing.*;
+import java.awt.*;
 
-public class Lamina_Botones {
+public class Lamina_Botones extends JPanel{
+	
+	private JButton[] botones;
+	private final String[] rotulos = {"%","C","^","/","7","8","9","x","4","5","6","-","1","2","3","+","+/-","0",",","="};
+	
+	public Lamina_Botones() {
+		
+		setLayout(new GridLayout(5,4,5,5));
+		
+		this.botones = new JButton[this.rotulos.length];
+		
+		agregarBotones(this.rotulos);
+	}
+	
+	private void agregarBotones(String[] rotulos) {
+		
+		for(int i = 0; i < rotulos.length; i++) {
+			
+			this.botones[i] = new JButton(rotulos[i]);
+			add(this.botones[i]);
+			
+		}
+		
+	}
 
 }
--- src/dom/Marco_Principal.java
+++ src/dom/Marco_Principal.java
@@ -1,14 +1,32 @@
 package dom;
 import javax.swing.*;
+import java.awt.*;
 
 public class Marco_Principal extends JFrame{
+	
+	private JLabel pantalla;
 	
 	public Marco_Principal() {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(350,500);
 		setTitle("Calculadora");
+		setLayout(new BorderLayout());
 		
+		//Pantalla de la calculadora
+		
+		pantalla = new JLabel("0", SwingConstants.RIGHT);
+		pantalla.setFont(new Font("Arial",Font.BOLD,80));
+		
+		add(pantalla, BorderLayout.NORTH);
+		
+		// Botones de la calculadora
+		
+		Lamina_Botones botones = new Lamina_Botones();
+		add(botones, BorderLayout.CENTER);
+		add(new JPanel(),BorderLayout.WEST);
+		add(new JPanel(),BorderLayout.EAST);
+		add(new JPanel(),BorderLayout.SOUTH);
 		
 	}
 
